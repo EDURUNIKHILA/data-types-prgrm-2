99e8e398a40a8da52e3a80cae00060607c5b2102
EDURUNIKHILA <115149482+EDURUNIKHILA@users.noreply.github.com>
2022-10-06 18:29:13 +0530

    Add files via upload

---
+++ Numeric.java
@@ -0,0 +1,21 @@
+package DataTypes;
+
+public class Numeric {
+
+	public static void main(String[] args) {
+		byte number=2;
+		short number1=6;
+		int number2=-3678;
+		long number3=-3456789L;
+		float number4=6.8f;
+		double number5=8.934;
+		System.out.println(number);
+		System.out.println(number1);
+		System.out.println(number2);
+		System.out.println(number3);
+		System.out.println(number4);
+		System.out.println(number5);
+
+	}
+
+}
